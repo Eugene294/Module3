6cc44b8f49b6d36a3111b218cecd427846d5984a
Eugene294 <koblia.eo@gmail.com>
2024-05-15 19:08:21 +1000

    Добавлен файл 3.3.2

---
+++ task3.3.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3._3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                string MyName = "Eugene";
+                byte MyAge = 25;
+                bool HaveIApet = false;
+                double MyShoeSize = 44.5;
+
+                Console.WriteLine("My name is " + MyName);
+                Console.WriteLine("MyAge " + MyAge);
+                Console.WriteLine("Do I have a pet? " + HaveIApet);
+                Console.WriteLine("My shoe size is " + MyShoeSize);
+
+            }
+    }
+}
